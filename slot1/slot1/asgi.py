caba00f4e12fb4d3eb8d62599a4da521585843b9
Priyadharshini565-hash <priyarameshhh565@gmail.com>
2024-11-25 03:44:16 +0530

    Success

--- slot1/slot1/asgi.py
+++ slot1/slot1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for slottimetable project.
+ASGI config for slot1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slottimetable.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slot1.settings')
 
 application = get_asgi_application()
--- slot1/slottimetable/apps.py
+++ slot1/slottimetable/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TimetableConfig(AppConfig):
+class SlottimetableConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timetable'
+    name = 'slottimetable'
